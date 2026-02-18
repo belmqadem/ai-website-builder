520413ada37192f86a929faac72b914e5b973f38
belmqadem <adilfront3@gmail.com>
2026-02-18 20:46:25 +0000

    08: messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -11,28 +11,35 @@ const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess() {
-        toast.success("Background job invoked successfully!");
+        toast.success("Message created successfully!");
       },
       onError() {
-        toast.error("Failed to invoke background job.");
+        toast.error("Failed to create message.");
       },
     }),
   );
 
   return (
     <div className="p-4 max-w-7xl mx-auto flex flex-col gap-4">
-      <Input className="p-4" value={value} onChange={(e) => setValue(e.target.value)} />
+      <Input
+        className="p-4"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <Button
-        disabled={invoke.isPending}
+        disabled={createMessage.isPending}
         onClick={() => {
-          invoke.mutate({ value: value });
+          createMessage.mutate({ value: value });
         }}
       >
         Submit
       </Button>
+
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 };
